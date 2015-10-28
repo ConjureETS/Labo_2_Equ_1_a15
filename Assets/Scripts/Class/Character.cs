1ec4de623173ec2ed842f6c730e8a54abc37b7c4
Marc-Antoine Dumont <dumont.marcantoine13@gmail.com>
2015-10-27 22:47:35 -0400

    Initial commit

---
+++ Assets/Scripts/Class/Character.cs
@@ -0,0 +1,27 @@
+﻿
+public class Character{
+
+    private int mass;
+    private int mouvementSpeed { get; set; }
+    private int forceJump { get; set; }
+
+
+    public Character(int m, int mvtSpeed, int fJump)
+    {
+        this.mass = m;
+        this.mouvementSpeed = mvtSpeed;
+        this.forceJump = fJump;
+    }
+
+
+    public int getMass()
+    {
+        return mass;
+    }
+    public void setMass(int m)
+    {
+        this.mass = m;
+    }
+
+
+}
---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+    public Character player;
+	
+	// Update is called once per frame
+	void Update () {
+        int test = player.getMass();
+	}
+}
