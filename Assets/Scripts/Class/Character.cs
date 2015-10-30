4a2a0fd111dccc6cae0d31780ec42c5a38090e5d
Marc-Antoine Dumont <dumont.marcantoine13@gmail.com>
2015-10-29 20:42:26 -0400

    First part

--- Assets/Scripts/Class/Character.cs
+++ Assets/Scripts/Class/Character.cs
@@ -4,13 +4,15 @@ public class Character{
     private int mass;
     private int mouvementSpeed { get; set; }
     private int forceJump { get; set; }
+    private bool touchingTheFloor;
 
 
-    public Character(int m, int mvtSpeed, int fJump)
+    public Character(int m, int mvtSpeed, int fJump, bool touchFloor)
     {
         this.mass = m;
         this.mouvementSpeed = mvtSpeed;
         this.forceJump = fJump;
+        this.touchingTheFloor = touchFloor;
     }
 
 
@@ -21,6 +23,14 @@ public class Character{
     public void setMass(int m)
     {
         this.mass = m;
+    }
+
+    public void setTouchingTheFloor(bool touchFloor){
+        this.touchingTheFloor = touchFloor;
+    }
+    public bool isTouchingTheFloor()
+    {
+        return touchingTheFloor;
     }
 
 
--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -4,9 +4,49 @@ using System.Collections;
 public class Gravity : MonoBehaviour {
 
     public Character player;
+    void Start()
+    {
+        player = new Character(1, 10, 10, false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        int test = player.getMass();
+
+        if (!player.isTouchingTheFloor())
+        {
+            this.gameObject.transform.Translate(0,-0.1f,0);
+        }
+
+        if (Input.GetKey("p"))
+        {
+            player.setTouchingTheFloor(false);
+        }
 	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            player.setTouchingTheFloor(true);
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            
+            player.setTouchingTheFloor(false);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            player.setTouchingTheFloor(true);
+        }
+    }
 }
