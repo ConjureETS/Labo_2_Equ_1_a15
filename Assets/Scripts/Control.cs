a7c78fc497a8bb46582e46185e0d53af75654db0
Marc-Antoine Dumont <dumont.marcantoine13@gmail.com>
2015-10-29 20:44:15 -0400

    Add scene for each personne

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+
+
+    public Transform thingsToMove;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float mouvement = Input.GetAxis ("Horizontal");
+
+        if ( mouvement!= 0)
+        {
+            thingsToMove.Translate(new Vector2(mouvement * speed, 0));
+        }
+	
+	}
+}
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+    public int timeJump;
+
+    public Transform player;
+
+    private bool isJumping;
+	// Use this for initialization
+	void Start () {
+        isJumping = false;
+        timeJump = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Fire1") && isJumping == false)
+        {
+            timeJump = 50;
+            isJumping = true;
+        }
+
+        if (timeJump > 0) {
+            player.Translate(0, 0.5f, 0);
+            timeJump--;      
+        }
+        else
+        {
+            isJumping = false;
+        }
+	
+	}
+}
