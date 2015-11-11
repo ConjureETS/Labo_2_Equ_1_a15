8426a784f801122b6f3cc68bda70d304bb747a0d
FrancisGallant <gallanker@gmail.com>
2015-11-11 18:42:02 -0500

    prefab ennemy et floor pour patrol

---
+++ Assets/Scripts/Patrolling.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrolling : MonoBehaviour {
+
+    
+    private float direction = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(direction, 0, 0);
+	}
+
+    void FixedUpdate() {
+    }
+
+    public void setDirection(float value) {
+        direction = value;
+    }
+
+    public float getDirection() {
+        return direction;
+    }
+
+}
