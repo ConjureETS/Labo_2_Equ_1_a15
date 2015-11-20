0819f51529928348b4e90d98cf7bd8735cb4abe6
jventalon <justine.ventalon@utbm.fr>
2015-11-19 21:24:31 -0500

    Player health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -11,42 +11,62 @@ public class Health : MonoBehaviour
 
 	private ArrayList hearts = new ArrayList();
 
+	// Width (X) and height (Y) of an heart image with additional blank space
 	private float spaceX;
 	private float spaceY;
 
 	void Start () 
 	{
-		RectTransform r = heartImage.rectTransform;
-		spaceX = r.rect.width;
-		spaceY = r.rect.height;
-		Debug.Log("spaceX : " + spaceX);
-		Debug.Log("spaceY : " + spaceY);
+		spaceX = 2.3f;
+		spaceY = 2.3f;
+
 		AddHearts (startingHealth);
 	}
 
-	void AddHearts (int n) 
+	public void AddHearts (int n) 
 	{
 		for (int i = 0; i < n; i++) 
 		{
 			Transform heart = ((Image)Instantiate(heartImage)).transform;
-			heart.SetParent(GameObject.Find("HeartCanvas").transform, false);
 
+			// Add heart image to the canvas
+			heart.SetParent(GameObject.Find("Canvas").transform, false);
+
+			// Calculate x and y position of the new heart (in number of hearts)
 			int y = Mathf.FloorToInt(hearts.Count/maxHeartsPerRow);
 			int x = hearts.Count - y * maxHeartsPerRow;
 
-			Debug.Log("X : " + x);
-			Debug.Log("Y : " + y);
-
 			Vector2 pos = heart.position;
 			pos.x += x * spaceX;
-			pos.y += y * spaceY;
-
-			Debug.Log("posX : " + pos.x);
-			Debug.Log("posY : " + pos.y);
+			pos.y += -y * spaceY;
 
 			heart.position = pos;
 
+			// Add heart
 			hearts.Add(heart);
+		}
+	}
+
+	public void LoseHearts(int n)
+	{
+		for (int i = 0; i < n && i < hearts.Count; i++) 
+		{
+			int lastIndex = hearts.Count - 1;
+
+			// Destroy the image of the last heart
+			Image heartImage = (Image)((Transform)hearts[lastIndex]).GetComponent("Image");
+			Destroy(heartImage);
+
+			// Remove the last heart
+			hearts.RemoveAt(lastIndex);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) 
+	{ 
+		if( collision.gameObject.tag == "Ennemy" )
+		{
+			LoseHearts(1);
 		}
 	}
 }
